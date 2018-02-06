fc62dcee4cee9887095b823b3ad56ab9561dd527
SnowmanNunu <345750542@qq.com>
2018-02-06 03:58:03 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -31,7 +32,17 @@ class UsersController extends Controller
         'email'=>$requset->email,
         'password'=>bcrypt($requset->password),
       ]);
+
+      Auth::login($user);
       session()->flash('success','Welcome to weibo!');
       return redirect()->route('users.show',[$user]);
     }
+
+    public function destroy()
+    {
+      Auth::logout();
+      session()->flash('success','You have exited succefully!');
+      return redirect('login');
+    }
+
 }
